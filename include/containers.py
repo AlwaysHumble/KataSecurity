acf7b6c6a2063f0d5071c89f1c272df8bdb71685
AlwaysHumble <naidu.karan10@gmail.com>
2020-10-28 05:03:08 +0530

    Small Changes ONly

--- include/containers.py
+++ include/containers.py
@@ -1,43 +1,45 @@
 import os
 import subprocess
 
-MAIN_PATH = "/home/karan/KataSecurity"
-PYTHON_VERSION = "python3.8"
-TEST_CASES_PATH = r"/home/karan/KataSecurity/test_cases"
-NUMBER_OF_CONTAINERS = len(os.listdir(TEST_CASES_PATH))
-
 class Container:
 
     def __init__(self):
         self.testcaseslist = []
+        self.curr_dir = os.getcwd()
+        #print(self.curr_dir)
+        self.test_cases_path = self.curr_dir+"/test_cases"
+        self.include_folder = self.curr_dir+"/include"
+        self.shellscripts = self.include_folder+"/ShellScripts"
+        self.container_make = self.shellscripts+"/make_containers.sh"
+        self.container_test = self.shellscripts+"/test_scripts.sh"
+        self.container_remove = self.shellscripts+"/remove_containers.sh"
+        self.number_of_containers = len(os.listdir(self.test_cases_path))
         self.containersmade = 0
 
     def get_test_cases(self):
-        self.testcaseslist = os.listdir(TEST_CASES_PATH)
+        self.testcaseslist = os.listdir(self.test_cases_path)
         
-
     def make_containers(self):
         self.get_test_cases()
-        for i in range(NUMBER_OF_CONTAINERS):
+        for i in range(self.number_of_containers):
             container_name = "Container_"+str(i+1)
             test_case_name = self.testcaseslist[i]
-            testpath = "/home/karan/KataSecurity/test_cases/"+test_case_name
-            subprocess.call(["bash","/home/karan/KataSecurity/include/ShellScripts/make_containers.sh",container_name,test_case_name],shell=False)
+            subprocess.call(["bash",self.container_make,container_name,test_case_name],shell=False)
             
         self.containersmade = 1
 
     def test_containers(self):
         if(self.containersmade):
-            for i in range(NUMBER_OF_CONTAINERS):
+            for i in range(self.number_of_containers):
                 container_name = "Container_"+str(i+1)
                 test_case_name = self.testcaseslist[i]
-                subprocess.call(["bash","/home/karan/KataSecurity/include/ShellScripts/test_scripts.sh",container_name,test_case_name],shell=False)
+                subprocess.call(["bash",self.container_test,container_name,test_case_name],shell=False)
 
     def remove_containers(self):
         if(self.containersmade):
-            for i in range(NUMBER_OF_CONTAINERS):
+            for i in range(self.number_of_containers):
                 container_name = "Container_"+str(i+1)
-                subprocess.call(["bash","/home/karan/KataSecurity/include/ShellScripts/remove_containers.sh",container_name],shell=False)
+                subprocess.call(["bash",self.container_remove,container_name],shell=False)
         self.containersmade = 0
 
     def initial_test(self):
